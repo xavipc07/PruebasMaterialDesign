9b33515b8f8444fb3d75e58583e8e90cf63d10f1
xavipc07 <xaverius00@gmail.com>
2017-05-05 13:30:11 -0500

    Clase para hacer pruebas de Layout

--- app/src/main/java/com/apps/xavipc/pruebasmaterialdesign/NuevosLayout.java
+++ app/src/main/java/com/apps/xavipc/pruebasmaterialdesign/NuevosLayout.java
@@ -1,7 +1,18 @@
 package com.apps.xavipc.pruebasmaterialdesign;
 
 
-public class NuevosLayout {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class NuevosLayout extends AppCompatActivity{
+
     public NuevosLayout() {
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
