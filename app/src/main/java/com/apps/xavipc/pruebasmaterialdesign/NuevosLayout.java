02340523afb68bb6a7eeb663b3dc6a9c15601191
xavipc07 <xaverius00@gmail.com>
2017-05-05 14:02:39 -0500

    Clase para hacer pruebas de Layout

--- app/src/main/java/com/apps/xavipc/pruebasmaterialdesign/NuevosLayout.java
+++ app/src/main/java/com/apps/xavipc/pruebasmaterialdesign/NuevosLayout.java
@@ -4,6 +4,7 @@ package com.apps.xavipc.pruebasmaterialdesign;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class NuevosLayout extends AppCompatActivity{
 
@@ -13,6 +14,6 @@ public class NuevosLayout extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Toast.makeText(this, "Esto es una prueba", Toast.LENGTH_SHORT).show();
     }
 }
